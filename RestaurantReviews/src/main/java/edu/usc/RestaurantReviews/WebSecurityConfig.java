2640bc890461093c82084c2e22740e87eda18bfe
Sarah Nakama <snakama@usc.edu>
2022-11-20 14:42:55 -0800

    return 401 error instead of login page

--- RestaurantReviews/src/main/java/edu/usc/RestaurantReviews/WebSecurityConfig.java
+++ RestaurantReviews/src/main/java/edu/usc/RestaurantReviews/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +14,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
  
 @Configuration
 @EnableWebSecurity
@@ -49,19 +53,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
  
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable() //to make postman requests
+    	http.exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+        http
+        .cors().and().csrf().disable() //to make postman requests - TODO actually implement cors
         	.authorizeRequests()
         	.antMatchers("/api/**").authenticated()
             .and()
             .formLogin()
             .loginPage("/login")
                 .usernameParameter("username")
-                .defaultSuccessUrl("/api/restaurants")
                 .permitAll()
             .and()
             .logout()
-            .logoutUrl("/logout")
-            .logoutSuccessUrl("/").permitAll();
+            .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
     }
      
      
