7a029a09d19ec63d363c4ade4ad7ea3223186d89
Aditya Kumar <adityak2003@Adityas-MacBook-Air.local>
2022-11-21 17:41:19 -0800

    fixed CORS policy

--- RestaurantReviews/src/main/java/edu/usc/RestaurantReviews/RestaurantController.java
+++ RestaurantReviews/src/main/java/edu/usc/RestaurantReviews/RestaurantController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(maxAge = 3600)
 public class RestaurantController {
 	
 	@Autowired RestaurantService rService;
--- RestaurantReviews/src/main/java/edu/usc/RestaurantReviews/ReviewController.java
+++ RestaurantReviews/src/main/java/edu/usc/RestaurantReviews/ReviewController.java
@@ -3,6 +3,7 @@ package edu.usc.RestaurantReviews;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 //import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(maxAge = 3600)
 public class ReviewController {
 	@Autowired
     ReviewService revService;
