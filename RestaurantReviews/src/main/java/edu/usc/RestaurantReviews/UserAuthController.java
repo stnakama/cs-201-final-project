86790bef61d6f7c6c805bcd8d47fe8c70a9cbbcf
Aditya Kumar <adityak2003@Adityas-MacBook-Air.local>
2022-11-21 16:35:51 -0800

    added getUsername endpoint

--- RestaurantReviews/src/main/java/edu/usc/RestaurantReviews/UserAuthController.java
+++ RestaurantReviews/src/main/java/edu/usc/RestaurantReviews/UserAuthController.java
@@ -48,4 +48,10 @@ public class UserAuthController {
 	public boolean isUserLoggedIn(){	
 		return !SecurityContextHolder.getContext().getAuthentication().getName().equals("anonymousUser");
 	}
+	
+	@GetMapping("/getUsername")
+	@ResponseBody
+	public String getUsername(){	
+		return SecurityContextHolder.getContext().getAuthentication().getName();
+	}
 }
